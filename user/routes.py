a2ad94302742d5cce90ba71f50455a34ea329ed7
Vaishnavi Sawant <101731164+VS1901@users.noreply.github.com>
2024-04-09 13:13:49 +0530

    Add files via upload

---
+++ user/routes.py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect
+from app import app
+from user.models import User
+from threading import Thread
+from mic import transcript, sumarizer
+
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
+    return User().signup()
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    return User().login()
+
+@app.route('/user/logout')
+def logout():
+    return User().logout()
+
+@app.route('/main/transcript', methods=('GET','POST'))
+def transcription():
+    thread = Thread(target=transcript)
+    thread.start()
+    return ('', 204)
+
+@app.route('/main/transcriptstop', methods=('GET','POST'))
+def transcriptionstop():
+    global go
+    go = False
+    print("ABC")
+    sumarizer()
+    return redirect('/main/')
